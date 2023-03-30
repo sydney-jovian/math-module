ed2329d2cc1c7894ddd1670fdc1f6b5f1ebb4979
sydney-jovian <126050345+sydney-jovian@users.noreply.github.com>
2023-03-30 15:57:54 +0000

    Add multiply.js and release 1.1.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const sum = require("./lib/sum.js");
 const subtract = require("./lib/subtract.js");
+const multiply = require("./lib/multiply.js");
 
 
-module.exports = { sum, subtract };
+module.exports = { sum, subtract, multiply };
